b391be6ea24c57ee27cfe5b9233ae14806526bbb
gogohhh <gogo_hhh_d@hotmail.com>
2022-02-11 16:03:48 -0600

    no ecto

--- mix.exs
+++ mix.exs
@@ -36,8 +36,9 @@ defmodule KajoolyTemplate.MixProject do
     [
       {:phoenix, "~> 1.6.6"},
       {:phoenix_html, "~> 3.0"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
+      #{:phoenix_ecto, "~> 4.4"},
+      #{:ecto_sql, "~> 3.6"},
+      #{:postgrex, ">= 0.0.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
